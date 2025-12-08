9a0fd45062cc1f05d07e6d922fbbfc664b50bd7b
houssem Eddine <houssem_eddine94@hotmail.com>
2025-12-07 20:10:35 -0500

    Creation du projet

--- HoussemTester/HoussemTester/Program.cs
+++ HoussemTester/HoussemTester/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.IO;
+
 
 namespace HoussemTester
 {
@@ -11,8 +14,19 @@ namespace HoussemTester
 
         static void Main(string[] args)
         {
+
+            string[] lines = File.ReadAllLines("input.txt");
+
+            int position = 50;
+
+            int NbDeZero = 0;
+
+
+
         }
 
-        
+
+
+
     }
 }
